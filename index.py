a65d01b751e0d3f4be6fa92824b985269b9228dc
TheLoneWolf316 <TheLoneWolf316@users.noreply.github.com>
2018-04-22 15:49:39 -0400

    changed var name

--- index.py
+++ index.py
@@ -1,3 +1,3 @@
 # List of copypastas shown when entering the '!shitscord copypasta list' command
-cpnames = ["navyseals", "buzzwords", "nicejoke", "navyfurs", "navyknights", "navyswag", "navykindergarten", "navyweeb",
+cplibrary = ["navyseals", "buzzwords", "nicejoke", "navyfurs", "navyknights", "navyswag", "navykindergarten", "navyweeb",
            "imnotgay", "ipadgirl", "extrathicc", "theintent", "whitehouse"]
